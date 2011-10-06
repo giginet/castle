a2ffcf734c4ef97d305b1f741d08d9f0e56fcf8a
giginet <giginet.net@gmail.com>
2011-10-06 16:49:27 +0900

    setup_package.sh書いてる

---
+++ setup_package.sh
@@ -0,0 +1,9 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+
+# install python support vim
+brew install git://gist.github.com/721952.git +python +python3
+
+# for vim plugins
+brew install node
+brew install yuicompressor
