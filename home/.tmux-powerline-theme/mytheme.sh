bcc047520b5bc64f1baacbf627f1da696f1c6889
giginet <giginet.net@gmail.com>
2013-11-27 08:31:04 +0900

    Add iTunes status to tmux-powerline.

--- home/.tmux-powerline-theme/mytheme.sh
+++ home/.tmux-powerline-theme/mytheme.sh
@@ -29,6 +29,7 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
+		"pwd 89 211" \
 		"vcs_branch 29 88" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
@@ -40,9 +41,8 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
 		#"mailcount 9 255" \
-		#"now_playing 234 37" \
+		"now_playing 234 37" \
 		#"cpu 240 136" \
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
