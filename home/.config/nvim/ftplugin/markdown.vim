70eaf2b756bb5d4d2ee769d15eec58562beb77a5
giginet <giginet.net@gmail.com>
2024-04-21 16:44:15 +0900

    Add obsidian.nvim

--- home/.config/nvim/ftplugin/markdown.vim
+++ home/.config/nvim/ftplugin/markdown.vim
@@ -1,3 +1,4 @@
 set tabstop=4
 set shiftwidth=4
 set wrap
+setl conceallevel=2
--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -308,4 +308,22 @@ return require('lazy').setup({
       vim.cmd.colorscheme 'tokyonight-night'
     end
   },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "Documents",
+          path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents",
+        },
+      },
+    },
+  },
 })
