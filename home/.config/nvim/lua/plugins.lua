49d3c494cc999fbd6b392ef7193036386972c00e
giginet <giginet.net@gmail.com>
2024-07-21 00:45:55 +0900

    Add xcodebuild.nvim

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -104,6 +104,19 @@ return require('lazy').setup({
     end,
   },
   {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-tree.lua",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("xcodebuild").setup({
+    })
+  end,
+  },
+  {
     'tfnico/vim-gradle',
     ft = {'gradle'},
   },
