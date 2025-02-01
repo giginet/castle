716a5b39c611ec95e1ca586fb33279ab4dc8ba89
giginet <giginet.net@gmail.com>
2025-02-02 01:47:29 +0900

    Migrate markdown ftplugin to Lua

---
+++ home/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+vim.opt_local.conceallevel = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.wrap = true
+vim.opt_local.conceallevel = 2
+vim.keymap.set('n', '<C-x><C-x>', function()
+  local current_file = vim.fn.expand('%:p')
+  local cmd = '/usr/bin/open -a "Marked 2.app" ' .. vim.fn.shellescape(current_file)
+  vim.fn.system(cmd)
+end, { buffer = 0, noremap = true, silent = true, desc = 'Open current file in Marked 2' })
